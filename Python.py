7f09b32452c482e0d5bb5ae3925434f3a781248b
Icheka <rhemafortune@gmail.com>
2021-01-21 23:40:21 +0100

    Added the source code for implementing the algorithm in Python.

---
+++ Python.py
@@ -0,0 +1,24 @@
+class Sort:
+    def _swap(self, arr, idx1, idx2):
+        if (arr[idx1] > arr[idx2]):
+            tmp = arr[idx1]
+            arr[idx1] = arr[idx2]
+            arr[idx2] = tmp
+
+        return arr
+    
+    def bubbleSort(self, arr):
+        for i in range(0, len(arr) - 1):
+            for idx in range(0, len(arr) - 1):
+                arr = self._swap(arr, idx, idx + 1)
+            
+        return arr
+
+
+def main():
+    arr = [3,5,1,-3,4,8,7,9]
+    sort = Sort()
+    sorted_arr = sort.bubbleSort(arr)
+    return sorted_arr
+
+print("Sorted list: {}".format(main()))
